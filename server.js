14e824a4c9105f6eb54294bfa215ec0a1678abd4
Gil Chen-Zion <gc2466@columbia.edu>
2013-07-06 11:06:36 -0400

    Set up env. Less working. Jade Working. GAHHHHHHH

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require('express');
-var app = express();
 var mongoose = require('mongoose');
+var app = express();
 
 app.configure(function() {
-	app.use(express.bodyParser());
+	
 	app.set('views', __dirname + '/views');
   	app.set('view engine', 'jade');
+  	app.set('view options', { layout: false });
+  	app.use(express.bodyParser());
+  	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use("/public", express.static( __dirname + '/public'));
 });
 
 mongoose.connect('mongodb://localhost/login');
